44b5c68379eac3631b2a8f3da1eb65691019e726
capjamesg <jamesg@jamesg.blog>
2024-06-21 18:52:19 +0100

    remove unused files

--- title_normalizer/__init__.py
+++
@@ -1,5 +0,0 @@
-from .titles import IncompleteRequestError, URLRetrievalError, get_normalized_title
-
-__all__ = ["get_normalized_title", "IncompleteRequestError", "URLRetrievalError"]
-
-__version__ = "0.0.1"
